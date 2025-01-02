7ed9096a9400c8a1e8485f5796eba32e22f51605
baharkarabulut <baharkarabulut25@gmail.com>
2025-01-02 18:27:33 +0300

    Project Status chart completed.

--- FrmAnasayfa.Designer.cs
+++ FrmAnasayfa.Designer.cs
@@ -31,11 +31,19 @@
 			DevExpress.XtraCharts.SimpleDiagram3D simpleDiagram3D1 = new DevExpress.XtraCharts.SimpleDiagram3D();
 			DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
 			DevExpress.XtraCharts.Pie3DSeriesView pie3DSeriesView1 = new DevExpress.XtraCharts.Pie3DSeriesView();
+			DevExpress.XtraCharts.XYDiagram3D xyDiagram3D1 = new DevExpress.XtraCharts.XYDiagram3D();
+			DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
+			DevExpress.XtraCharts.SideBySideBar3DSeriesView sideBySideBar3DSeriesView1 = new DevExpress.XtraCharts.SideBySideBar3DSeriesView();
 			this.chartToplamGelir = new DevExpress.XtraCharts.ChartControl();
+			this.chartProjeDurumu = new DevExpress.XtraCharts.ChartControl();
 			((System.ComponentModel.ISupportInitialize)(this.chartToplamGelir)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(simpleDiagram3D1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(pie3DSeriesView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.chartProjeDurumu)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(xyDiagram3D1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(series2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(sideBySideBar3DSeriesView1)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// chartToplamGelir
@@ -45,7 +53,8 @@
     "73411242764804;-0.397702775529513;0;0.36345909755089;0.161447056750991;0.9175142" +
     "13663168;0;0;0;0;1";
 			this.chartToplamGelir.Diagram = simpleDiagram3D1;
-			this.chartToplamGelir.Location = new System.Drawing.Point(12, 12);
+			this.chartToplamGelir.Location = new System.Drawing.Point(9, 10);
+			this.chartToplamGelir.Margin = new System.Windows.Forms.Padding(2);
 			this.chartToplamGelir.Name = "chartToplamGelir";
 			this.chartToplamGelir.PaletteBaseColorNumber = 2;
 			this.chartToplamGelir.PaletteName = "Concourse";
@@ -55,15 +64,33 @@
 			series1.View = pie3DSeriesView1;
 			this.chartToplamGelir.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
         series1};
-			this.chartToplamGelir.Size = new System.Drawing.Size(505, 362);
+			this.chartToplamGelir.Size = new System.Drawing.Size(379, 294);
 			this.chartToplamGelir.TabIndex = 0;
+			// 
+			// chartProjeDurumu
+			// 
+			xyDiagram3D1.RotationMatrixSerializable = "-0.964051479177289;0.243979518882632;-0.10525559301981;0;0.226692368403619;0.9618" +
+    "40478757345;0.153210520302815;0;0.138619319013292;0.123842189054065;-0.982572031" +
+    "256068;0;0;0;0;1";
+			this.chartProjeDurumu.Diagram = xyDiagram3D1;
+			this.chartProjeDurumu.Location = new System.Drawing.Point(393, 10);
+			this.chartProjeDurumu.Name = "chartProjeDurumu";
+			series2.Name = "Series2";
+			series2.SeriesID = 1;
+			series2.View = sideBySideBar3DSeriesView1;
+			this.chartProjeDurumu.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series2};
+			this.chartProjeDurumu.Size = new System.Drawing.Size(379, 294);
+			this.chartProjeDurumu.TabIndex = 1;
 			// 
 			// FrmAnasayfa
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(2008, 770);
+			this.ClientSize = new System.Drawing.Size(1506, 626);
+			this.Controls.Add(this.chartProjeDurumu);
 			this.Controls.Add(this.chartToplamGelir);
+			this.Margin = new System.Windows.Forms.Padding(2);
 			this.Name = "FrmAnasayfa";
 			this.Text = "Anasayfa";
 			this.Load += new System.EventHandler(this.FrmAnasayfa_Load);
@@ -71,6 +98,10 @@
 			((System.ComponentModel.ISupportInitialize)(pie3DSeriesView1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.chartToplamGelir)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(xyDiagram3D1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(sideBySideBar3DSeriesView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(series2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.chartProjeDurumu)).EndInit();
 			this.ResumeLayout(false);
 
 		}
@@ -78,5 +109,6 @@
 		#endregion
 
 		private DevExpress.XtraCharts.ChartControl chartToplamGelir;
+		private DevExpress.XtraCharts.ChartControl chartProjeDurumu;
 	}
 }
--- FrmAnasayfa.cs
+++ FrmAnasayfa.cs
@@ -30,15 +30,33 @@ namespace ProFin
 				return toplamGelir;
 			}
 		}
+		private int GetTamamlananProjeSayisi()
+		{
+			using (var dbContext = new DbProFinEntities())
+			{
+				return dbContext.Projeler.Count(p => p.Durum == "Tamamlandı");
+			}
+		}
+
+		private int GetDevamEdenProjeSayisi()
+		{
+			using (var dbContext = new DbProFinEntities())
+			{
+				return dbContext.Projeler.Count(p => p.Durum == "Devam Ediyor");
+			}
+		}
+
+
 		private void FrmAnasayfa_Load(object sender, EventArgs e)
 		{
+			// Gelir-Gider Grafiği
 			Series series = new Series("Gelir-Gider", ViewType.Pie3D);
 
 			SeriesPoint gelirPoint = new SeriesPoint("Gelir", GetToplamGelir());
-			gelirPoint.Color = Color.DarkGreen;
+			gelirPoint.Color = ColorTranslator.FromHtml("#9bcd9b");
 
 			SeriesPoint giderPoint = new SeriesPoint("Gider", 60000);
-			giderPoint.Color = Color.Red;
+			giderPoint.Color = ColorTranslator.FromHtml("#ee5c42");
 
 			series.Points.Add(gelirPoint);
 			series.Points.Add(giderPoint);
@@ -53,8 +71,27 @@ namespace ProFin
 
 			ChartTitle chartTitle = new ChartTitle();
 			chartTitle.Text = "Toplam Gelir/Gider";
-			chartToplamGelir.Titles.Clear();  // Eski başlıkları temizle
+			chartToplamGelir.Titles.Clear(); 
 			chartToplamGelir.Titles.Add(chartTitle);
+
+			// Proje Durumu Grafiği
+			Series projeDurumSeries = new Series("Proje Durumu", ViewType.Bar);
+			projeDurumSeries.Points.Add(new SeriesPoint("Tamamlanan Projeler", GetTamamlananProjeSayisi()));
+			projeDurumSeries.Points.Add(new SeriesPoint("Devam Eden Projeler", GetDevamEdenProjeSayisi()));
+			projeDurumSeries.Points[0].Color = ColorTranslator.FromHtml("#519fdb");
+			projeDurumSeries.Points[1].Color = ColorTranslator.FromHtml("#f9f995"); 
+
+
+			chartProjeDurumu.Series.Clear();
+			chartProjeDurumu.Series.Add(projeDurumSeries);
+
+			SideBySideBarSeriesView barView = (SideBySideBarSeriesView)projeDurumSeries.View;
+			barView.ColorEach = true;
+
+			ChartTitle projeDurumTitle = new ChartTitle { Text = "Proje Durumu Grafiği" };
+			chartProjeDurumu.Titles.Clear();
+			chartProjeDurumu.Titles.Add(projeDurumTitle);
+
 		}
 	}
 }
